1de500a191d6f3ede3214f49aa36230723470252
Vaibhav Patil <107416335+Vaiibhavv@users.noreply.github.com>
2024-02-26 09:39:46 +0000

    logical_operators added

---
+++ Linux_series/logical_operator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# - "||" =logical or
+# - "&&" = logical and
+# - "!" = logical not
+
+echo "College fees status"
+read -p "Please enter a amount you have make : " stuAmt
+echo "enter month in lowercae"
+read -p 'Please enter payment month : ' stuMnt
+fix_amount=45000
+month="january"
+
+if [ $stuAmt -eq $fix_amount ]  && [ $stuMnt == $month ]
+then 
+  echo "You are eligle"
+else
+  echo "You are not eligible"
+fi
