289cd021e3658372a06b06fb4d2b0d8033efef4c
Vaibhav Patil <107416335+Vaiibhavv@users.noreply.github.com>
2024-02-27 11:16:53 +0000

    counter example

---
+++ Linux_series/while_loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+num=1
+while [ $num -le 10 ]
+do 
+   echo $num
+  # num=`expr $num + 1`
+  #(( num++ ))
+  num=$(( num + 1 ))
+done
+
+## print 2s table
+num=1
+while [ $num -le 10 ]
+do 
+   echo $(( num * 2 ))
+  # num=`expr $num + 1`
+  #(( num++ ))
+  num=$(( num + 1 ))
+done
